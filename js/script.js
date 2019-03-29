71cb8f1e509afe21ab4994261fdae8c8ad470487
Amine <aminx4@gmail.com>
2019-03-29 16:48:12 +0100

    exo de mercredi

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   setTimeout(function(){
+      popUp();
+   },5000); // 5000 to load it after 5 seconds from page load
+});
+function popUp(){
+        document.getElementById('ac-wrapper').style.display="none"; 
+
+}
+
